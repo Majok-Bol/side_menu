c81a585646a41b8d0d4866ec9206fb7b5c079e24
Majok-Bol <bolmajok001@gmail.com>
2025-04-08 15:13:12 +0300

    removed debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home:Scaffold(
         appBar: AppBar(
           title: Text('Side menu',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,
