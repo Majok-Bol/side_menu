0555890427d33cf7ed7a1ee98b3982629bdd42b3
Majok-Bol <bolmajok001@gmail.com>
2025-04-08 15:22:28 +0300

    added side menu items

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,20 @@ class MyApp extends StatelessWidget{
         appBar: AppBar(
           title: Text('Side menu',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,
         ),
+        body: Center(child: Text('Main context'),),
+        drawer: Drawer(
+          child: ListView(
+            children:const [
+            DrawerHeader(decoration:BoxDecoration(
+              color: Colors.blueGrey,
+            ),
+                child: Text('Menu')),
+              ListTile(title: Text('Home'),),
+              ListTile(title: Text('Settings'),),
+              ListTile(title: Text('Logout'),),
+            ],
+          ),
+        ),
       ) ,
     );
   }
